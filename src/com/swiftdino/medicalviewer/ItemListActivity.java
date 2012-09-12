6c855b9704cd0668dcc5cca8c61c4dc4173ce198
gremsi26 <gremsi26@gmail.com>
2012-09-12 16:46:25 -0400

    removed override

--- src/com/swiftdino/medicalviewer/ItemListActivity.java
+++ src/com/swiftdino/medicalviewer/ItemListActivity.java
@@ -24,7 +24,6 @@ public class ItemListActivity extends FragmentActivity
         }
     }
 
-    @Override
     public void onItemSelected(String id) {
         if (mTwoPane) {
             Bundle arguments = new Bundle();
--- src/com/swiftdino/medicalviewer/ItemListFragment.java
+++ src/com/swiftdino/medicalviewer/ItemListFragment.java
@@ -23,7 +23,7 @@ public class ItemListFragment extends ListFragment {
     }
 
     private static Callbacks sDummyCallbacks = new Callbacks() {
-        @Override
+        
         public void onItemSelected(String id) {
         }
     };
