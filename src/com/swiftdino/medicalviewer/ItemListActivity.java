a4d2c382236f70bc08e94084246253a5965dcafa
Vishnu Rajeevan <vishnu@localhost.localdomain>
2012-09-15 16:21:27 -0400

    fixing merge breakages

--- src/com/swiftdino/medicalviewer/ItemListActivity.java
+++ src/com/swiftdino/medicalviewer/ItemListActivity.java
@@ -1,4 +1,3 @@
-
 package com.swiftdino.medicalviewer;
 
 import android.content.Intent;
@@ -41,54 +40,4 @@ public class ItemListActivity extends FragmentActivity
             startActivity(detailIntent);
         }
     }
-}
-=======
-package com.swiftdino.medicalviewer;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.util.Log;
-import android.view.MenuItem;
-
-public class ItemListActivity extends FragmentActivity implements ItemListFragment.Callbacks {
-
-    private boolean mTwoPane;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_twopane);
-
-        if (findViewById(R.id.info_display_container) != null) {
-            mTwoPane = true;
-            ((ItemListFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.item_list))
-                    .setActivateOnItemClick(true);
-        }
-        
-        
-    }
-
-    public void onItemSelected(String id) {
-        
-    	if (mTwoPane) {
-        	
-            //Bundle arguments = new Bundle();
-            //arguments.putString(ItemDetailFragment.ARG_ITEM_ID, id);
-            InfoDisplayFragment fragment = new InfoDisplayFragment();
-            //fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.info_display_container, fragment)
-                    .commit();
-
-        } else {
-            Intent detailIntent = new Intent(this, InfoDisplayActivity.class);
-            //detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
-            startActivity(detailIntent);
-        }
-        
-    }
-    
 }
