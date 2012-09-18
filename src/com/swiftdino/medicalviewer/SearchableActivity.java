28a5189402b8ad40e1e5d86933ad49c66754f33d
gremsi26 <gremsi26@gmail.com>
2012-09-17 22:31:52 -0400

    - Can go back to previous screen from search screen.

--- src/com/swiftdino/medicalviewer/SearchableActivity.java
+++ src/com/swiftdino/medicalviewer/SearchableActivity.java
@@ -4,7 +4,9 @@ import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class SearchableActivity extends ListActivity {
@@ -13,6 +15,9 @@ public class SearchableActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.searchable);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
 		Log.d(TAG, "Loaded searchable.xml");
 		// Get the intent, verify the action and get the query
 		Intent intent = getIntent();
@@ -20,12 +25,22 @@ public class SearchableActivity extends ListActivity {
 
 	}
 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			NavUtils.navigateUpTo(this, new Intent(this,
+					MasterListActivity.class));
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
 	private void handleIntent(Intent intent) {
 		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
 			String query = intent.getStringExtra(SearchManager.QUERY);
 			// doMySearch(query);
-			Toast.makeText(this, "Searching for: " + query + "...",
-					Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "Searching for: " + query, Toast.LENGTH_SHORT)
+					.show();
 
 			Log.d(TAG, "Searching for " + query);
 		}
