d27b88c37e14a7e11e60f1073b6403ad1d6ed259
gremsi26 <gremsi26@gmail.com>
2012-09-21 17:18:32 -0400

    final fix

--- src/com/swiftdino/medicalviewer/DetailViewActivity.java
+++ src/com/swiftdino/medicalviewer/DetailViewActivity.java
@@ -1,6 +1,5 @@
 package com.swiftdino.medicalviewer;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -9,43 +8,44 @@ import android.util.Log;
 import android.view.MenuItem;
 
 public class DetailViewActivity extends FragmentActivity {
-final String TAG = "DetailViewActivity";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        
-        Log.d(TAG, "Setting Content View");
-        setContentView(R.layout.detail_view_fragment);
+	final String TAG = "DetailViewActivity";
 
-        Log.d(TAG, "Getting Intent");
-        Intent launchingIntent = getIntent();
-        String content = launchingIntent.getData().toString();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        Log.d(TAG, "Content: "+content);
-        Log.d(TAG, "Starting detail view fragment");
-        DetailViewFragment detailView = (DetailViewFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_detailview);
-        
-    //    Bundle arguments = new Bundle();
-      //  arguments.putString(DetailViewFragment.ARG_ITEM_ID,
-        //        getIntent().getStringExtra(DetailViewFragment.ARG_ITEM_ID));
-        //detailView.setArguments(arguments);
-        
-        Log.d(TAG, "Updating detail view fragment");
-        detailView.updateGraph(content);
-        
-        
-    }
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            NavUtils.navigateUpTo(this, new Intent(this, MasterListActivity.class));
-            return true;
-        }
+		Log.d(TAG, "Setting Content View");
+		setContentView(R.layout.detail_view_fragment);
 
-        return super.onOptionsItemSelected(item);
-    }
+		Log.d(TAG, "Getting Intent");
+		Intent launchingIntent = getIntent();
+		String content = launchingIntent.getData().toString();
+
+		Log.d(TAG, "Content: " + content);
+		Log.d(TAG, "Starting detail view fragment");
+		DetailViewFragment detailView = (DetailViewFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.fragment_detailview);
+
+		// Bundle arguments = new Bundle();
+		// arguments.putString(DetailViewFragment.ARG_ITEM_ID,
+		// getIntent().getStringExtra(DetailViewFragment.ARG_ITEM_ID));
+		// detailView.setArguments(arguments);
+
+		Log.d(TAG, "Updating detail view fragment");
+		detailView.updateGraph(content);
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			NavUtils.navigateUpTo(this, new Intent(this,
+					MasterListActivity.class));
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 }
