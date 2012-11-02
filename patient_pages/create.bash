b897fc37f8eaa0b455b9a70713b61f83e177e898
Vishnu Rajeevan <vishnu@localhost.localdomain>
2012-11-02 01:18:03 -0400

    testing patient page creation stuff

--- patient_pages/create.bash
+++ patient_pages/create.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 c=1
-while [ $c -le 2 ]
+while [ $c -le 19 ]
 do
 		filename="patient$c.html"
 		echo $filename
 		touch $filename
 		echo "This is patient $c" > $filename
+		cat div.html | tee -a $filename
 		(( c++ ))
 done
 
