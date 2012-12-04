128ee68631791643ad17a590a3228a3b3e4913ee
anjan <anjan@katana.(none)>
2012-12-04 16:01:08 -0500

    Debugging jqplot

--- patient_pages/grapher.js
+++ patient_pages/grapher.js
@@ -1,7 +1,6 @@
 function plot(points){
 	document.write( '<script language="javascript" src="../jqplot/jquery.min.js" />' );
 	document.write( '<script language="javascript" src="../jqplot/jquery.jqplot.min.js" />' );
-	alert("LOL");
 	var plot3 = $.jqplot('chartdiv', [points], {  
 		series:[{showMarker:false}],
 		axes:{
